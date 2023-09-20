b03c795696fb12762b385d074838df4017220a8b
smige1 <122740136+smige1@users.noreply.github.com>
2023-09-21 00:27:11 +0100

    Update write_history.c

--- write_history.c
+++ write_history.c
@@ -10,7 +10,7 @@ char *get_history_file(info_t *info)
 {
 	char *buf, *dir;
 
-	dir = getenvi(info, "HOME=");
+	dir = _getenv(info, "HOME=");
 	if (!dir)
 		return (NULL);
 	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
