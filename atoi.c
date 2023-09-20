2ef63961b28f69a5c983d2868a438442ee2416e4
smige1 <egimsikana@gmail.com>
2023-09-20 08:22:42 -0700

    done

--- atoi.c
+++ atoi.c
@@ -4,55 +4,62 @@
  * interactive - returns true if shell is interactive mode
  * @info: struct address
  *
- * is_delim - checks if character is a delimeter
- * @delim: the delimeter string
- * _isalpha - checks for alphabetic character
- * @v: The character to input
- * _atoi - converts a string to an integer
- * @x: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
+ * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
-
-int is_del(char v, char *del)
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
 {
-	while (*del)
-		if (*del++ == v)
+	while (*delim)
+		if (*delim++ == c)
 			return (1);
 	return (0);
 }
 
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
 
-
-int alpha(int v)
+int _isalpha(int c)
 {
-	if ((v >= 'a' && v <= 'z') || (v >= 'A' && v <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
 
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ */
 
-
-int atoi(char *x)
+int _atoi(char *s)
 {
-	int y, sign = 1, flag = 0, output;
+	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (y = 0; x[y] != '\0' && flag != 2; y++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
-		if (x[y] == '-')
+		if (s[i] == '-')
 			sign *= -1;
 
-		if (x[y] >= '0' && x[y] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (x[y] - '0');
+			result += (s[i] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
