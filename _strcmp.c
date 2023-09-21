bc2d23a390f99dba400919d4e7c777b53fe43efe
smige1 <122740136+smige1@users.noreply.github.com>
2023-09-21 03:10:45 +0100

    Update _strcmp.c

--- _strcmp.c
+++ _strcmp.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
+ * _strcmp - performs lexicogarphic___
  * @s1: the first strang
  * @s2: the second strang
  *
- * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ * Return: negative if s1 < s2++
  */
 int _strcmp(char *s1, char *s2)
 {
