1f26f0cc89d4468df0c43546b53f1c14eaaccee5
smige1 <122740136+smige1@users.noreply.github.com>
2023-09-21 03:20:47 +0100

    Update chain.c

--- chain.c
+++ chain.c
@@ -28,18 +28,34 @@
  * @new: new string
  *
  * Return: 1 if replaced, 0 otherwise
- */
-
-#include "shell.h"
-
-/**
+ * check_chain - checks we should continue chaining based on last status
+ * @info: the parameter struct
+ * @buf: the char buffer
+ * @p: address of current position in buf
+ * @i: starting position in buf
+ * @len: length of buf
+ *
  * is_chain - test if current char in buffer is a chain delimeter
  * @info: the parameter struct
  * @buf: the char buffer
  * @p: address of current position in buf
  *
- * Return: 1 if chain delimeter, 0 otherwise
+ * replace_alias - replaces an aliases in the tokenized string
+ * @info: the parameter struct
+ *
+ * replace_vars - replaces vars in the tokenized string
+ * @info: the parameter struct
+ *
+ * replace_string - replaces string
+ * @old: address of old string
+ * @new: new string
+ *
+ * Return: 1 if replaced, 0 otherwise
  */
+
+#include "shell.h"
+
+
 int is_chain(info_t *info, char *buf, size_t *p)
 {
 	size_t j = *p;
@@ -67,16 +83,7 @@ int is_chain(info_t *info, char *buf, size_t *p)
 	return (1);
 }
 
-/**
- * check_chain - checks we should continue chaining based on last status
- * @info: the parameter struct
- * @buf: the char buffer
- * @p: address of current position in buf
- * @i: starting position in buf
- * @len: length of buf
- *
- * Return: Void
- */
+
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 {
 	size_t j = *p;
@@ -101,19 +108,14 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 	*p = j;
 }
 
-/**
- * replace_alias - replaces an aliases in the tokenized string
- * @info: the parameter struct
- *
- * Return: 1 if replaced, 0 otherwise
- */
+
 int replace_alias(info_t *info)
 {
-	int i;
+	int z;
 	list_t *node;
 	char *p;
 
-	for (i = 0; i < 10; i++)
+	for (z = 0; z < 10; z++)
 	{
 		node = node_starts_with(info->alias, info->argv[0], '=');
 		if (!node)
@@ -130,12 +132,7 @@ int replace_alias(info_t *info)
 	return (1);
 }
 
-/**
- * replace_vars - replaces vars in the tokenized string
- * @info: the parameter struct
- *
- * Return: 1 if replaced, 0 otherwise
- */
+
 int replace_vars(info_t *info)
 {
 	int i = 0;
@@ -172,13 +169,7 @@ int replace_vars(info_t *info)
 }
 
 
-/**
- * replace_string - replaces string
- * @old: address of old string
- * @new: new string
- *
- * Return: 1 if replaced, 0 otherwise
- */
+
 int replace_string(char **old, char *new)
 {
 	free(*old);
