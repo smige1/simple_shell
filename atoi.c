70e98c3469b26e372dfb72238ab39f3609dbf1d3
smige1 <122740136+smige1@users.noreply.github.com>
2023-09-21 03:16:59 +0100

    Update atoi.c

--- atoi.c
+++ atoi.c
@@ -5,18 +5,25 @@
  * @info: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
  */
+
+
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
-/**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
- */
+
 int is_delim(char c, char *delim)
 {
 	while (*delim)
@@ -25,11 +32,6 @@ int is_delim(char c, char *delim)
 	return (0);
 }
 
-/**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
- * Return: 1 if c is alphabetic, 0 otherwise
- */
 
 int _isalpha(int c)
 {
@@ -39,27 +41,22 @@ int _isalpha(int c)
 		return (0);
 }
 
-/**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
- */
 
 int _atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
+	int z, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	for (z = 0; s[z] != '\0' && flag != 2; z++)
 	{
-		if (s[i] == '-')
+		if (s[z] == '-')
 			sign *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[z] >= '0' && s[z] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (s[i] - '0');
+			result += (s[z] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
