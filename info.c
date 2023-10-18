3ce12363b39061953fdcdc6efeb78697adb2af56
smige1 <egimsikana@gmail.com>
2023-10-18 14:00:12 -0700

    done

--- info.c
+++ info.c
@@ -4,103 +4,30 @@
  * clear_info - initializes info_t struct
  * @info: struct address
  */
-void clear_info(info_t *info)
-{
-	info->arg = NULL;
-	info->argv = NULL;
-	info->path = NULL;
-	info->argc = 0;
+int execute_command(char *cmd, char **args);
+
+int main() {
+  char *cmd = "ls -l /tmp";
+  char *args[] = {"ls", "-l", "/tmp", NULL};
+
+  /*Execute the command*/
+  execute_command(cmd, args);
+
+  return 0;
 }
 
-/**
- * set_info - initializes info_t struct
- * @info: struct address
- * @av: argument vector
- */
-void set_info(info_t *info, char **av)
-{
-	int i = 0;
-
-	info->fname = av[0];
-	if (info->arg)
-	{
-		info->argv = strtow(info->arg, " \t");
-		if (!info->argv)
-		{
-			info->argv = malloc(sizeof(char *) * 2);
-			if (info->argv)
-			{
-				info->argv[0] = _strdup(info->arg);
-				info->argv[1] = NULL;
-			}
-		}
-		for (i = 0; info->argv && info->argv[i]; i++)
-			;
-		info->argc = i;
-
-		replace_alias(info);
-		replace_vars(info);
-	}
+int execute_command(char *cmd, char **args) {
+  pid_t pid = fork();
+  if (pid == 0) {
+    /* Child process*/
+    execve(cmd, args, environ);
+    exit(1);
+  } else if (pid > 0) {
+    /* Parent process*/
+    waitpid(pid, NULL, 0);
+    return 0;
+  } else {
+    /* Error forking*/
+    return -1;
+  }
 }
-
-/**
- * free_info - frees info_t struct fields
- * @info: struct address
- * @all: true if freeing all fields
- */
-void free_info(info_t *info, int all)
-{
-	ffree(info->argv);
-	info->argv = NULL;
-	info->path = NULL;
-	if (all)
-	{
-		if (!info->cmd_buf)
-			free(info->arg);
-		if (info->env)
-			free_list(&(info->env));
-		if (info->history)
-			free_list(&(info->history));
-		if (info->alias)
-			free_list(&(info->alias));
-		ffree(info->environ);
-			info->environ = NULL;
-		bfree((void **)info->cmd_buf);
-		if (info->readfd > 2)
-			close(info->readfd);
-		_putchar(BUF_FLUSH);
-	}
-}
-
-
-/**
- * free_list - frees all nodes of a list
- * @head_ptr: address of pointer to head node
- *
- * Return: void
- */
-void free_list(list_t **head_ptr)
-{
-	list_t *node, *next_node, *head;
-
-	if (!head_ptr || !*head_ptr)
-		return;
-	head = *head_ptr;
-	node = head;
-	while (node)
-	{
-		next_node = node->next;
-		free(node->str);
-		free(node);
-		node = next_node;
-	}
-	*head_ptr = NULL;
-}
-/**
- * **strtow - splits a string into words. Repeat delimiters are ignored
- * @str: the input string
- * @d: the delimeter string
- * Return: a pointer to an array of strings, or NULL on failure
- */
-
-
